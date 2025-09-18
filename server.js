8b277158db96e782358663e2156ac6d40cec3e51
zurg3 <20664125+zurg3@users.noreply.github.com>
2025-09-18 15:55:20 +0300

    Added server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const hostname = '127.0.0.1';
+const port = process.env.PORT || 3000;
+
+app.use(express.static('dist'));
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
