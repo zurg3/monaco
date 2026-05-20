bf0f2cb726479e454519a4e57629680060d754d3
zurg3 <20664125+zurg3@users.noreply.github.com>
2026-05-20 18:00:21 +0300

    Edited index.js

--- index.js
+++ index.js
@@ -44,6 +44,7 @@ const editor = monaco.editor.create(container, {
   guides: {
     indentation: false
   },
+  hideCursorInOverviewRuler: true,
   insertSpaces: true,
   lineHeight: 1.45,
   lineNumbers: 'on',
@@ -55,8 +56,14 @@ const editor = monaco.editor.create(container, {
   mouseWheelZoom: true,
   multiCursorModifier: 'ctrlCmd',
   occurrencesHighlight: 'off',
+  renderControlCharacters: false,
   renderLineHighlight: 'line',
   renderWhitespace: 'none',
+  rulers: [80],
+  scrollbar: {
+    horizontalScrollbarSize: 8,
+    verticalScrollbarSize: 8
+  },
   scrollBeyondLastLine: false,
   selectionHighlight: false,
   showDeprecated: false,
